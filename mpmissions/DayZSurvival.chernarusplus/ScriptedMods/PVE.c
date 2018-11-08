901a39a2469fdd680506339ea66a4753449bac18
fallingsheep <razorsoftinfo@gmail.com>
2018-11-09 10:37:06 +1100

    started PVE

---
+++ mpmissions/DayZSurvival.chernarusplus/ScriptedMods/PVE.c
@@ -0,0 +1,16 @@
+	override void EEHitBy(TotalDamageResult damageResult, int damageType, EntityAI source, int component, string dmgZone, string ammo, vector modelPos)
+	{
+		super.EEHitBy(damageResult, damageType, source, component, dmgZone, ammo, modelPos);
+		//get damage source name
+		name = source.GetDisplayName();
+		Print("damage source" + name);
+		
+		//will negate player damage here
+		//damageResult = 0;
+		
+	}
+	
+	override void EEHitByRemote(int damageType, EntityAI source, int component, string dmgZone, string ammo, vector modelPos)
+	{
+		super.EEHitByRemote(damageType, source, component, dmgZone, ammo, modelPos);
+	}
